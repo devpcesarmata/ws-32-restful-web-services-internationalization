b395c214aca12c3fc4c2af05f5a06e14af82f1b3
César <dev.pcesarmata@gmail.com>
2020-08-25 15:02:45 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws32restfulwebservicesinternationalization/Ws32RestfulWebServicesInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws32restfulwebservicesinternationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws32RestfulWebServicesInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws32RestfulWebServicesInternationalizationApplication.class, args);
+	}
+
+}
