0e4de7c73c484675e5b3e44d58067e61e5ef3a50
César <dev.pcesarmata@gmail.com>
2020-08-25 16:13:21 -0500

    Se concluye clase 32

--- src/main/java/com/cesarmata/ws32restfulwebservicesinternationalization/Ws32RestfulWebServicesInternationalizationApplication.java
+++ src/main/java/com/cesarmata/ws32restfulwebservicesinternationalization/Ws32RestfulWebServicesInternationalizationApplication.java
@@ -1,7 +1,13 @@
 package com.cesarmata.ws32restfulwebservicesinternationalization;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Ws32RestfulWebServicesInternationalizationApplication {
@@ -9,5 +15,18 @@ public class Ws32RestfulWebServicesInternationalizationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Ws32RestfulWebServicesInternationalizationApplication.class, args);
 	}
-
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
